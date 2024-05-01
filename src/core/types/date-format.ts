89fd36d2166f5edc7d9b099eb0da780433f3f2a5
Badreddine Ibril <badreddineibril@gmail.com>
2024-05-01 22:23:09 +0100

    Update "DateFomat" types

--- src/core/types/date-format.ts
+++ src/core/types/date-format.ts
@@ -1,4 +1,11 @@
 export interface DateFormat {
   format: string,
   preview: string,
+  type: "dash" | "slash" | "dot" | "comma" | "empty" | "HHmm",
+  functions: FunctionDefinition[]
+}
+
+export interface FunctionDefinition {
+  language_id: string,
+  function: string
 }
