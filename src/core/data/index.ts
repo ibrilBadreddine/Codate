0bd81afaa6f817cc7e2cba1241e37b19ee0b7d3c
Badreddine Ibril <badreddineibril@gmail.com>
2024-05-01 13:54:57 +0100

    Setup "Languages" data

---
+++ src/core/data/index.ts
@@ -0,0 +1,5 @@
+/**
+ * 
+ * Export all data
+ */
+export * from './languages';
---
+++ src/core/data/languages/index.ts
@@ -0,0 +1,68 @@
+import type { Language } from "@/core/types";
+import JavascriptIcon from "@/assets/icons/javascript.svg";
+import PythonIcon from "@/assets/icons/python.svg";
+import JavaIcon from "@/assets/icons/java.svg";
+import CSharpeIcon from "@/assets/icons/c#.svg";
+import RubyIcon from "@/assets/icons/ruby.svg";
+import PhpIcon from "@/assets/icons/php.svg";
+import SwiftIcon from "@/assets/icons/swift.svg";
+import KotlinIcon from "@/assets/icons/kotlin.svg";
+import GoIcon from "@/assets/icons/go.svg";
+import RustIcon from "@/assets/icons/rust.svg";
+
+/**
+ *
+ * Languages
+ */
+export const LANGUAGES: Language[] = [
+  {
+    id: "javascript",
+    name: "Javascript",
+    icon: JavascriptIcon,
+  },
+  {
+    id: "python",
+    name: "Python",
+    icon: PythonIcon,
+  },
+  {
+    id: "java",
+    name: "Java",
+    icon: JavaIcon,
+  },
+  {
+    id: "c#",
+    name: "C#",
+    icon: CSharpeIcon,
+  },
+  {
+    id: "ruby",
+    name: "Ruby",
+    icon: RubyIcon,
+  },
+  {
+    id: "php",
+    name: "Php",
+    icon: PhpIcon,
+  },
+  {
+    id: "swift",
+    name: "Swift",
+    icon: SwiftIcon,
+  },
+  {
+    id: "kotlin",
+    name: "Kotlin",
+    icon: KotlinIcon,
+  },
+  {
+    id: "go",
+    name: "Go",
+    icon: GoIcon,
+  },
+  {
+    id: "rust",
+    name: "Rust",
+    icon: RustIcon,
+  },
+];
