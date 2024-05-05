fc0a6511b0eeffa59c9e14b497c8e94d5ebab6bf
Badreddine Ibril <badreddineibril@gmail.com>
2024-05-05 17:41:53 +0100

    Make the lang selector dynamic

--- src/views/pages/home/index.tsx
+++ src/views/pages/home/index.tsx
@@ -2,12 +2,23 @@ import "./style.scss";
 import Hero from "./partials/Hero";
 import Selector from "./partials/Selector";
 import Formats from "./partials/formats";
+import { useState } from "react";
 
-export default function index() {
+export default function Index() {
+  /**
+   * 
+   * Programming language (default - "js")
+   */
+  const DEFAULT_LANG = "js"; 
+  const [selectedLang, setLang] = useState<string>(DEFAULT_LANG);
+
   return (
     <div className="home-page">
       <Hero />
-      <Selector />
+      <Selector 
+        selectedLang={selectedLang}
+        setLang={setLang}
+      />
       <Formats />
     </div>
   )
--- src/views/pages/home/partials/Selector.tsx
+++ src/views/pages/home/partials/Selector.tsx
@@ -1,12 +1,22 @@
 import { LANGUAGES } from "@/core/data";
 
-export default function Selector() {
+interface SelectorProps {
+  selectedLang: string;
+  setLang: (newLang: string) => void;
+}
+
+export const Selector: React.FC<SelectorProps> = ({selectedLang, setLang}) => {
   return (
     <section className="selector-container">
       {/* Programming Languages */}
       <div className="langs-selector">
         {LANGUAGES.map((lang) => (
-          <button key={lang.id} className="lang-btn" data-name={lang.name}>
+          <button
+            key={lang.id}
+            onClick={() => setLang(lang.id)}
+            className={selectedLang === lang.id ? "lang-btn active-lang" : "lang-btn"}
+            data-name={lang.name}
+          >
             <img src={lang.icon} alt={lang.name} className="icon" />
             <span className="name">{lang.name}</span>
           </button>
@@ -14,4 +24,6 @@ export default function Selector() {
       </div>
     </section>
   );
-}
+};
+
+export default Selector;
