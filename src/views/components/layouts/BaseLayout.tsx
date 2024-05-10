c42963de7a8ebc91e441fb67c81abae07f4178ac
Badreddine Ibril <badreddineibril@gmail.com>
2024-05-10 23:40:45 +0100

    Add Footer component

--- src/views/components/layouts/BaseLayout.tsx
+++ src/views/components/layouts/BaseLayout.tsx
@@ -1,13 +1,14 @@
 import "./style.scss";
 import { Outlet } from 'react-router-dom'
 import Header from "./_header";
+import Footer from "./_footer";
 
 export default function BaseLayout() {
   return (
     <div className='base-app'>
       <Header />
       <Outlet />
-      {/* Footer */}
+      <Footer />
     </div>
   )
 }
---
+++ src/views/components/layouts/_footer/index.tsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer>
+      <div className="owner">
+        <div className="icon">
+          <span className="material-symbols-outlined">calendar_today</span>
+        </div>
+        <p>
+          Developer by{" "}
+          <a href="https://bibril.com/" target="_blank">
+            @Badreddine Ibril
+          </a>
+        </p>
+      </div>
+      <div className="copyright">
+        <p>© {year} Fake Name All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
